ec95035b8e962971a05280d83128d5d12bc85f32
Maxence Coste <maxence.coste.mc@gmail.com>
2026-04-23 09:50:39 +0200

    feat: ajout du composant de liste de tâches

---
+++ src/app/components/task-list/task-list.ts
@@ -0,0 +1,20 @@
+import { Component, input, output } from '@angular/core';
+import { Task, TaskStatus } from '../../models/task.model';
+import { Category } from '../../models/category.model';
+import { TaskCard } from '../task-card/task-card';
+
+@Component({
+  selector: 'app-task-list',
+  imports: [TaskCard],
+  templateUrl: './task-list.html',
+  styleUrl: './task-list.css',
+})
+export class TaskList {
+  // API
+  tasks = input.required<Task[]>();
+  categories = input<Category[]>([]);
+
+  taskDeleted = output<string>();
+  taskEdited = output<Task>();
+  taskStatusChanged = output<{ id: string; status: TaskStatus }>();
+}
