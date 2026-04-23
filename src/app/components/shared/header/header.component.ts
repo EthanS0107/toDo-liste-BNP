90c23b5a60c99acde06d5e72569da2eb93a72215
Ethan Serville <ethan.serville@etudiant.univ-reims.fr>
2026-04-23 10:07:21 +0200

    feat: style header + contenue

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
-  selector: 'app-header.component',
-  imports: [],
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected taskService = inject(TaskService);
+}
