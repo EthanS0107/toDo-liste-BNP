aaf63d43aef16392c2ed4cfe1e39c78db43faee8
Ethan Serville <ethan.serville@etudiant.univ-reims.fr>
2026-04-27 10:20:17 +0200

    chore: utilisation du composent button dans le header

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TaskService } from '../../../services/task.service';
+import { ButtonComponent } from '../../ui/button/button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
