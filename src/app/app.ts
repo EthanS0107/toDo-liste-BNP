2f8042dc1031432fc2860f131df7ca58c9ba2bdc
Ethan Serville <ethan.serville@etudiant.univ-reims.fr>
2026-05-05 14:13:39 +0200

    feat : annimation de navigation entre les pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { trigger, transition, style, query, animate, group } from '@angular/animations';
 import { HeaderComponent } from './components/shared/header/header.component';
 
 @Component({
@@ -8,5 +9,42 @@ import { HeaderComponent } from './components/shared/header/header.component';
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  animations: [
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        style({ position: 'relative' }),
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              opacity: 0,
+            }),
+          ],
+          { optional: true },
+        ),
+        query(':enter', [style({ transform: 'translateY(20px)', opacity: 0 })], { optional: true }),
+        group([
+          query(
+            ':leave',
+            [animate('300ms ease-out', style({ transform: 'translateY(-20px)', opacity: 0 }))],
+            { optional: true },
+          ),
+          query(
+            ':enter',
+            [animate('300ms 150ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))],
+            { optional: true },
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
-export class App {}
+export class App {
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData;
+  }
+}
