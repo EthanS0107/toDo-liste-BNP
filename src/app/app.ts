4eba45fb5ce1ee71e27a513456cd45b76f835381
Maxence Coste <maxence.coste.mc@gmail.com>
2026-05-12 10:14:08 +0200

    fix: ajout des icones pour la tab bar et résolution de bug

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { ToastContainerComponent } from './components/ui/toast/toast-container.component';
+import { TabBarComponent } from './components/shared/tab-bar/tab-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ToastContainerComponent],
+  imports: [RouterOutlet, HeaderComponent, ToastContainerComponent, TabBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
